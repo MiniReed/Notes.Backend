19bc0eeb794baf68ec67bc5cb2fd322768f5d0da
максим <makkssim00@mail.ru>
2025-02-15 17:00:18 +0300

    change baseContr

--- Notes.WebApi/Controllers/BaseController.cs
+++ Notes.WebApi/Controllers/BaseController.cs
@@ -10,9 +10,7 @@ namespace Notes.WebApi.Controllers
     [Route("api/[controller]/[action]")]
     public abstract class BaseController : ControllerBase
     {
-        private IMediator _mediator;
-        protected IMediator Mediator =>
-            _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+
 
         internal Guid UserId => !User.Identity.IsAuthenticated
             ? Guid.Empty
